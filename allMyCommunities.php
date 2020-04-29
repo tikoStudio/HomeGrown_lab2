79e174f717e969c02be70a61c0c7210a05c67c87
tikoStudio <timothy.koenig@mail.com>
2020-04-29 16:23:22 +0200

    finished all my communities page

--- allMyCommunities.php
+++ allMyCommunities.php
@@ -23,7 +23,53 @@
 </head>
 
 <body>
+    <header>
+        <form action="" method="POST">
+            <div class="form__field form__field__search">
+                <img src="images/search.svg" alt="mail icon" class="form__icon">
+                <input type="text" id="search" name="search" placeholder="Look for specific farms">
+            </div>
+        </form>
+    </header>
 
+    <div class="community__container community__container--all">
+        <div class="community__title__container">
+            <h2>Current Communities</h2>
+        </div>
+        <?php foreach ($myCommunities as $community): ?>
+        <a href="community.php?com=<?php echo $community['id'] ?>"
+            class="community__data__container__a">
+            <div class="community__data__container community__data__container--all">
+                <div class="label">
+                    <p>Member</p>
+                </div>
+                <div class="community__img">
+                    <img src="images/<?php echo $community['img']; ?>"
+                        alt="farming resource picture">
+                </div>
+                <div class="community__info">
+                    <h3><?php echo $community['name']?>
+                    </h3>
+                    <?php if (!empty($community['crop1'])): ?>
+                    <div class=" farming">
+                        <p><?php echo $community['crop1'] ?>
+                        </p>
+                    </div>
+                    <?php endif; ?>
+                    <?php if (!empty($community['crop2'])): ?>
+                    <div class="farming">
+                        <p><?php echo $community['crop2'] ?>
+                        </p>
+                    </div>
+                    <?php endif; ?>
+                </div>
+                <p class=community__adress><?php echo $community['address'] ?>
+                </p>
+            </div>
+        </a>
+        <?php endforeach; ?>
+    </div>
+    <?php include_once("footer.inc.php"); ?>
 </body>
 
 </html>
